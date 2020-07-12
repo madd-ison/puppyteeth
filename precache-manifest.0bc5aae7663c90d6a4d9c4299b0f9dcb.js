20020b8d7e50a8ccaabeb488d0cf028e932462ab
Maddison Murphy <maddison@puppyteeth.org>
2020-07-12 14:47:50 -0400

    Updates

---
+++ precache-manifest.0bc5aae7663c90d6a4d9c4299b0f9dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fa338b8db54ee514caefdde4246d50",
+    "url": "/puppyteeth/index.html"
+  },
+  {
+    "revision": "91fd7696e7bbceab3b9d",
+    "url": "/puppyteeth/static/css/main.5e5c7de6.chunk.css"
+  },
+  {
+    "revision": "5b4dbdbe77e1e7281d59",
+    "url": "/puppyteeth/static/js/2.b96672d5.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/puppyteeth/static/js/2.b96672d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91fd7696e7bbceab3b9d",
+    "url": "/puppyteeth/static/js/main.ad22279c.chunk.js"
+  },
+  {
+    "revision": "101b857d516d35408702",
+    "url": "/puppyteeth/static/js/runtime-main.2e2f1127.js"
+  }
+]);
